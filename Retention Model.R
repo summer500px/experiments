7b1ae6a294900c7bd24cbdbd815306c3842aa346
summer500px <summer@500px.com>
2016-02-03 18:51:44 -0500

    Only one version (last) will be saved

--- Retention Model.R
+++ Retention Model.R
@@ -1,5 +1,7 @@
 # investigation of users registered before June 16
 # for experiment
+# whenever I make any changes
+# Would each of them be saved as an independent version?
 # ------------ <<Functions: Data Structurization for all users>> ------------
 
 # Step 1 - Getting cleaned full Registration Records
